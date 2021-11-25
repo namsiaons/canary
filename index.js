b66c912c98a9a95f6e52fe63957e45e619dd9f7f
basma jawadi <bbasma672@gmail.com>
2021-11-25 09:27:16 +0100

    Modification de projet

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const app = express()
 const port = 3000
 const path = require('path');
 app.use(express.static('public'));
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "canary",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/views/index.html'));
@@ -19,7 +32,17 @@ app.get('/signup', (req, res) => {
    
     res.sendFile(path.join(__dirname, '/views/signup.html'));
 })
-
+app.get('/profile/:username', (req, res) => {
+   
+   
+    res.sendFile(path.join(__dirname, '/views/profile.html'));
+   
+    console.log(req.params.username);
+    con.query("SELECT * FROM user_name where username=?",req.params.username, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+})
 
 
 app.listen(port, () => {
